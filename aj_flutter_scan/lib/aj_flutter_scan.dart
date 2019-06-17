b69258aa11235fa5276e591ce6770ebce0252075
chenhailong <chenhailong@anji-allways.com>
2019-06-17 16:00:08 +0800

    条形码识别优化

--- aj_flutter_scan/lib/aj_flutter_scan.dart
+++ aj_flutter_scan/lib/aj_flutter_scan.dart
@@ -6,8 +6,8 @@ class AjFlutterScan {
   static const MethodChannel _channel =
       const MethodChannel('aj_flutter_scan');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String> get getBarCode async {
+    final String version = await _channel.invokeMethod('getBarCode');
     return version;
   }
 }
