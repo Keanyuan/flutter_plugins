6c1688bf8ad554747dc2176425c34088e4e382b3
qizhiyuan <qizhiyuan@anji-plus.com>
2019-08-26 11:07:54 +0800

    add .h

--- aj_flutter_scan/ios/Classes/AjFlutterScanPlugin.h
+++ aj_flutter_scan/ios/Classes/AjFlutterScanPlugin.h
@@ -1,5 +1,7 @@
 #import <Flutter/Flutter.h>
 #import "BarcodeScannerViewControllerDelegate.h"
+#import "ZBarSDK.h"
+
 #define UIColorFromRGB(rgbValue) \
 [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
 green:((float)((rgbValue & 0x00FF00) >>  8))/255.0 \
