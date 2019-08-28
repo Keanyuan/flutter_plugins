41d1f57a56f36a194f3433b7140c6bd917ae8364
qizhiyuan <qizhiyuan@anji-plus.com>
2019-08-28 19:54:55 +0800

    add

---
+++ flutter_umeng_analytics/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <flutter_umeng_analytics/FlutterUmengAnalyticsPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterUmengAnalyticsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterUmengAnalyticsPlugin"]];
+}
+
+@end
