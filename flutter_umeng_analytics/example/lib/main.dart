03f0cfe6d7baa1106127f8d7fdd84905c1821d1f
qizhiyuan <qizhiyuan@anji-plus.com>
2019-08-29 10:46:10 +0800

    Android 添加logevent

--- flutter_umeng_analytics/example/lib/main.dart
+++ flutter_umeng_analytics/example/lib/main.dart
@@ -36,46 +36,38 @@ class _MyAppState extends State<MyApp> {
         appBar: new AppBar(
           title: new Text('Plugin example app'),
         ),
-        body: new Center(
-          child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                new RaisedButton(
-                  onPressed: () {
-                    UMengAnalytics.logEvent("hello");
-                  },
-                  child: new Text('Running on: hello'),
-                ),
+        body: ListView(
+            children: [
+              new RaisedButton(
+                onPressed: () {
+                  UMengAnalytics.logEvent("hello");
+                },
+                child: Container(alignment: Alignment.center,child: new Text('Running on: hello'),)
+              ),
 
-                new RaisedButton(
-                  onPressed: () {
-                    List list;
-                    print(list.length);
-                  },
-                  child: new Text('Running on: reportError 错误统计  添加一个错误信息 \n iOS 需要在友盟后台 添加一个reportError的事件'),
-                ),
-                Container(
-                  height: 200,
-                  child: Text("测试"),
-                ),
-                Container(
-                  height: 200,
-                  child: Text("测试"),
-                ),
-                Container(
-                  height: 200,
-                  child: Text("测试"),
-                ),
-                Container(
-                  height: 200,
-                  child: Text("测试"),
-                ),
-                Container(
-                  height: 200,
-                  child: Text("测试"),
-                ),
-              ]),
-        ),
+              new RaisedButton(
+                onPressed: () {
+                  List list;
+                  print(list.length);
+                },
+                child: new Text('Running on: reportError 错误统计  添加一个错误信息 \n iOS 需要在友盟后台 添加一个reportError的事件'),
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: 200,
+                child: Text("测试"),
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: 200,
+                child: Text("测试"),
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: 200,
+                child: Text("测试"),
+              ),
+            ]),
       ),
     );
   }
