d079335cbed4430a3c19f754f147806126825632
qizhiyuan <qizhiyuan@anji-plus.com>
2019-11-09 10:13:07 +0800

    ADD

---
+++ aj_flutter_pda/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <pda/PdaPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [PdaPlugin registerWithRegistrar:[registry registrarForPlugin:@"PdaPlugin"]];
+}
+
+@end
