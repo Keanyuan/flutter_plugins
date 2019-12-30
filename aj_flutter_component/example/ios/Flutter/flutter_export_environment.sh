3afc81ed08868a8ed9d3fa20ee30993380e375d5
qizhiyuan <qizhiyuan@anji-plus.com>
2019-12-30 10:12:27 +0800

    ad

--- aj_flutter_component/example/ios/Flutter/flutter_export_environment.sh
+++ aj_flutter_component/example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/kean_qi/flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/kean_qi/Desktop/AllwaysWork/AllwaysGit/yidong/aj_flutter_component/example"
-export "FLUTTER_TARGET=/Users/kean_qi/Desktop/AllwaysWork/AllwaysGit/yidong/aj_flutter_component/example/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/kean_qi/Desktop/AllwaysWork/AllwaysGit/yidong/modules/aj_flutter_component/example"
+export "FLUTTER_TARGET=/Users/kean_qi/Desktop/AllwaysWork/AllwaysGit/yidong/modules/aj_flutter_component/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/kean_qi/flutter/flutter/bin/cache/artifacts/engine/ios"
